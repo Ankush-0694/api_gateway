a45724feb654fd8add6457e85288eccfc197590d
Ankush Kumar <ankush@Ankushs-MacBook-Pro.local>
2021-05-19 22:19:52 +0530

    regular commit

--- server.js
+++ server.js
@@ -1,13 +1,34 @@
-const express = require("express");
-const app = express();
-const cors = require("cors");
+// const express = require("express");
+// const app = express();
+// const cors = require("cors");
 
-app.use(cors());
+// app.use(cors());
 
-app.get("/apiGateway", (req, res) => {
-  res.send("api_gateway service in your service");
+// app.get("/apiGateway", (req, res) => {
+//   res.send("api_gateway service in your service");
+// });
+
+// app.listen(4010, () => {
+//   console.log("server listening to port 4010");
+// });
+
+const { ApolloServer } = require("apollo-server");
+const { ApolloGateway } = require("@apollo/gateway");
+
+const port = 4010;
+
+const gateway = new ApolloGateway({
+  serviceList: [
+    { name: "product", url: "http://localhost:4001/graphql" },
+    // { name: "order", url: "http://localhost:4002" },
+  ],
 });
 
-app.listen(4010, () => {
-  console.log("server listening to port 4010");
+const server = new ApolloServer({
+  gateway,
+  subscriptions: false,
+});
+
+server.listen({ port }).then(({ url }) => {
+  console.log(`Apollo Server ready at url${url}`);
 });
