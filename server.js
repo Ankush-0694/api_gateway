4f174656c39f62e0917bcce416cf76d21671040e
Ankush Kumar <ankushkashyap0000@gmail.com>
2021-06-24 16:03:44 +0530

    express-jwt and buildService added for auth

--- server.js
+++ server.js
@@ -1,34 +1,56 @@
-// const express = require("express");
-// const app = express();
-// const cors = require("cors");
+const { ApolloServer } = require("apollo-server-express");
+const { ApolloGateway, RemoteGraphQLDataSource } = require("@apollo/gateway");
+const express = require("express");
+const expressJwt = require("express-jwt");
+require("dotenv").config();
 
-// app.use(cors());
-
-// app.get("/apiGateway", (req, res) => {
-//   res.send("api_gateway service in your service");
-// });
-
-// app.listen(4010, () => {
-//   console.log("server listening to port 4010");
-// });
-
-const { ApolloServer } = require("apollo-server");
-const { ApolloGateway } = require("@apollo/gateway");
-
+const app = express();
 const port = 4010;
+
+app.use(
+  expressJwt({
+    secret: process.env.JWT_SECRET,
+    algorithms: ["HS256"],
+    credentialsRequired: false,
+  })
+);
 
 const gateway = new ApolloGateway({
   serviceList: [
-    { name: "product", url: "http://localhost:4001/graphql" },
-    // { name: "order", url: "http://localhost:4002" },
+    { name: "products", url: "http://localhost:5000" },
+    { name: "orders", url: "http://localhost:5001" },
+    { name: "users", url: "http://127.0.0.1:4000/graphql" },
   ],
+
+  /**
+   * @param buildService this function add the user object to request header before sending request to the service
+   */
+  buildService({ name, url }) {
+    // console.log(`${name} ,  ${url}`);
+    return new RemoteGraphQLDataSource({
+      url,
+      willSendRequest({ request, context }) {
+        request.http.headers.set(
+          "user",
+          context.user ? JSON.stringify(context.user) : null
+        );
+      },
+    });
+  },
 });
 
 const server = new ApolloServer({
   gateway,
   subscriptions: false,
+  context: ({ req }) => {
+    const user = req.user || null;
+    // console.log(user);
+    return { user };
+  },
 });
 
-server.listen({ port }).then(({ url }) => {
-  console.log(`Apollo Server ready at url${url}`);
+server.applyMiddleware({ app }); // add this into product and order , and update url into service
+
+app.listen({ port }, () => {
+  console.log(`Server ready at http://localhost:${port}${server.graphqlPath}`);
 });
